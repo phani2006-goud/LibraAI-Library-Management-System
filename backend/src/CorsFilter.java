f749d88c1031c25c698768dc950440e56e747b4b
phani2006-goud <phanigoud1910@gmail.com>
2026-09-12 21:00:42 +0530

    Create CorsFilter.java

---
+++ backend/src/CorsFilter.java
@@ -0,0 +1,36 @@
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse httpResponse =
+                (HttpServletResponse) response;
+
+        httpResponse.setHeader(
+                "Access-Control-Allow-Origin", "*");
+
+        httpResponse.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS");
+
+        httpResponse.setHeader(
+                "Access-Control-Allow-Headers",
+                "Content-Type");
+
+        chain.doFilter(request, response);
+    }
+}
